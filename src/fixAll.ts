4b824bef7a1e03d84b4aa9a36bd79fd4479d8946
chemzqm <chemzqm@gmail.com>
2019-04-03 09:28:37 +0800

    udpate dependencies

--- src/fixAll.ts
+++ src/fixAll.ts
@@ -2,10 +2,10 @@ import { isTypeScriptDocument, isEnabledForJavaScriptDocument } from './utils'
 import { Range, CodeAction, TextDocument, Position, CodeActionContext, CancellationToken, CodeActionKind, Diagnostic } from 'vscode-languageserver-protocol'
 import { CodeActionProvider, languages } from 'coc.nvim'
 
-export async function getTsLintFixAllCodeAction(document: TextDocument, diagnostics: Diagnostic[]): Promise<CodeAction | undefined> {
+export async function getTsLintFixAllCodeAction(document: TextDocument, diagnostics: ReadonlyArray<Diagnostic>): Promise<CodeAction | undefined> {
   let end: Position = document.positionAt(document.getText().length)
   let range = Range.create(0, 0, end.line, end.character)
-  const map = await languages.getCodeActions(document, range, { diagnostics })
+  const map = await languages.getCodeActions(document, range, { diagnostics: diagnostics.slice() })
   const codeActions: CodeAction[] = []
   for (let item of map.values()) {
     codeActions.push(...item)
@@ -17,7 +17,7 @@ export async function getTsLintFixAllCodeAction(document: TextDocument, diagnost
 }
 
 export class FixAllProvider implements CodeActionProvider {
-  async provideCodeActions(
+  public async provideCodeActions(
     document: TextDocument,
     _range: Range,
     context: CodeActionContext,
