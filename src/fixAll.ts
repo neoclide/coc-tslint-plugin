e1dd438f88fe5cb51755450934d98bc3451e3802
Qiming Zhao <chemzqm@gmail.com>
2020-12-24 12:44:31 +0800

    update dependencies

--- src/fixAll.ts
+++ src/fixAll.ts
@@ -5,7 +5,7 @@ import { CodeActionProvider, languages } from 'coc.nvim'
 export async function getTsLintFixAllCodeAction(document: TextDocument, diagnostics: ReadonlyArray<Diagnostic>): Promise<CodeAction | undefined> {
   let end: Position = document.positionAt(document.getText().length)
   let range = Range.create(0, 0, end.line, end.character)
-  const map = await languages.getCodeActions(document, range, { diagnostics: diagnostics.slice() })
+  const map = await (languages as any).getCodeActions(document, range, { diagnostics: diagnostics.slice() })
   const codeActions: CodeAction[] = []
   for (let item of map.values()) {
     codeActions.push(...item)
